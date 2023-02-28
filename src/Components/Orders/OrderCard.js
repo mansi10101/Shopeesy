43b7a45a8b4bf83d1427eb486161a79b938f1283
mansi10101 <mansinagaria01@gmail.com>
2023-02-28 18:14:09 +0530

    Svg styling fixed

--- src/Components/Orders/OrderCard.js
+++ src/Components/Orders/OrderCard.js
@@ -5,37 +5,35 @@ import { Link } from 'react-router-dom';
 const OrderCard = ({id, name,size, img, price, quantity}) => {
 
     return (
-        
-        <div className={styles.Cart_box}>
-          <div className={styles.product}>
+      <div className={styles.Cart_box}>
+        <div className={styles.product}>
           <Link to={`/product/${id} `}>
             <div className={styles.p_img}>
               <img src={img} alt='' />
             </div>
-            </Link>
-            <div className={styles.p_details}>
+          </Link>
+          <div className={styles.p_details}>
             <Link to={`/product/${id} `}>
-              <div className={styles.item}>
+              <div className={styles.itemtitle}>
                 <h2>{name}</h2>
               </div>
-              </Link>
-              <div className={styles.item}>
-                <h2>${price}</h2>
-              </div>
-              <div className={styles.item}>
-                <h2>Quanity: {quantity}</h2>
-              </div>
-              <div className={styles.item}>
+            </Link>
+            <div className={styles.item}>
+              <h2>${price}</h2>
+            </div>
+            <div className={styles.item}>
+              <h2>Quanity: {quantity}</h2>
+            </div>
+            <div className={styles.item}>
               <h2>Size: {size}</h2>
-              </div>
-              <div className={styles.item}>
-                <button className={styles.remove_button}>Cancel Item</button>
-              </div>
+            </div>
+            <div className={styles.item}>
+              <button className={styles.remove_button}>Cancel Item</button>
             </div>
           </div>
         </div>
-     
-    )
+      </div>
+    );
 }
 
 export default OrderCard
